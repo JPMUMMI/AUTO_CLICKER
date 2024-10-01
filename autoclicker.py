43af7b6e2c598cb5f1ea1346b9a7504224518f2f
Juan M. Urbina <84646041+JPMUMMI@users.noreply.github.com>
2024-10-01 14:21:10 -0300

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,24 +1,33 @@
-##pip install pynput
 import time
 import threading
 from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode
+import sys
 
 clicking = False
 mouse = Controller()
+click_event = threading.Event()
 
 def clicker():
     while True:
-        if clicking:
-            mouse.click(Button.left, 1)
+        click_event.wait()
+        mouse.click(Button.left, 1)
         time.sleep(0.01)
 
 def toggle_event(key):
+    global clicking
     if key == KeyCode.from_char('c'):
-        global clicking
         clicking = not clicking
+        if clicking:
+            click_event.set()
+        else:
+            click_event.clear()
+    elif key == KeyCode.from_char('q'):
+        listener.stop()
+        sys.exit()
 
 click_thread = threading.Thread(target=clicker)
+click_thread.daemon = True
 click_thread.start()
 
 with Listener(on_press=toggle_event) as listener:
